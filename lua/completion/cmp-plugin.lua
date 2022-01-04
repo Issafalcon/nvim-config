8d32288c110a8659b5f4baa497f0d26940270235
Adam Figgins <adam_figgins@waters.com>
2022-01-04 16:43:36 +0000

    Nvim - Some more fixes for workign on WSL
    - Updated cmp menu so it doesn't render weirdly on WSL
    - Fixing ultest plugin mappings (remap option was incorrect)
    - Changing toggleterm mappings so selecting fzf options using c-j and
      c-k still works

--- lua/completion/cmp-plugin.lua
+++ lua/completion/cmp-plugin.lua
@@ -60,24 +60,16 @@ cmp.setup {
     )
   },
   formatting = {
-    fields = {"kind", "abbr", "menu"},
     format = lspkind.cmp_format(
       {
-        with_text = true, -- do not show text alongside icons
-        maxwidth = 100, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-        -- The function below will be called before any actual modifications from lspkind
-        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-        before = function(entry, vim_item)
-          vim_item.menu =
-            ({
-            nvim_lsp = "[LSP]",
-            vsnip = "[Snippet]",
+        with_text = true,
+          menu = ({
             buffer = "[Buffer]",
-            path = "[Path]",
-            npm = "[NPM]"
-          })[entry.source.name]
-          return vim_item
-        end
+            nvim_lsp = "[LSP]",
+            vsnip = "[VSnip]",
+            nvim_lua = "[Lua]",
+            latex_symbols = "[Latex]"
+          })
       }
     )
   },
