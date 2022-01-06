32910af9d5fc300aa9892f7de05e7708a0e8ad65
Adam Figgins <atfiggins@gmail.com>
2022-01-06 22:04:36 +0000

    Updating default colourscheme to codemonkey
    - Adding neosharper plugin (needs to be in local repos folder)

--- lua/colourscheme/colourscheme.lua
+++ lua/colourscheme/colourscheme.lua
@@ -37,6 +37,6 @@ require('material').setup({
 	} -- Overwrite highlights with your own
 })
 
-vim.cmd('colorscheme tomorrow')
+vim.cmd('colorscheme codemonkey')
 
 vim.api.nvim_set_keymap('n', '<leader>tm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]], { noremap = true, silent = true })
