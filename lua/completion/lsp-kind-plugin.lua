4b1a0f89aa69f858367f62a4a435ebd145d5f320
Adam Figgins <atfiggins@gmail.com>
2022-02-05 16:04:38 +0000

    Nvim - Fixing lsp-kind config

--- lua/completion/lsp-kind-plugin.lua
+++ lua/completion/lsp-kind-plugin.lua
@@ -1,8 +1,8 @@
 require('lspkind').init({
-    -- enables text annotations
-    --
-    -- default: true
-    with_text = true,
+    -- defines how annotations are shown
+    -- default: symbol
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+    mode = 'symbol',
 
     -- default symbol map
     -- can be either 'default' (requires nerd-fonts font) or
