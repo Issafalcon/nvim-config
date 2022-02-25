b2716344cf5af7a004ebb4c686883f255d38dd8b
Adam Figgins <adam_figgins@waters.com>
2022-02-25 15:21:33 +0000

    TS - Adding treesitter rainbow brackets

--- lua/syntax-highlighting/treesitter-plugin.lua
+++ lua/syntax-highlighting/treesitter-plugin.lua
@@ -6,6 +6,11 @@ require "nvim-treesitter.configs".setup {
     disable = {"latex"},
     additional_vim_regex_highlighting = true
   },
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil
+  },
   indent = {
     enable = true,
     disable = {
