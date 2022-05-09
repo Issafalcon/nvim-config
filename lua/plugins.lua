71c1b2d30c0e4c1c24c9955a4631af964ccb70db
Adam Figgins <adam_figgins@waters.com>
2022-05-09 09:03:53 +0100

    Revert "Nvim - Removing rnvimr plugin and shortcuts"
    This reverts commit bd1f7a4aa1a0d09ac77293e69f190305d838a93a.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -138,6 +138,7 @@ return packer.startup(function(use)
   use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
   -- File Navigation
+  use("kevinhwang91/rnvimr")
   use("kyazdani42/nvim-tree.lua")
   use("preservim/tagbar")
   use({
--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,5 +1,6 @@
 require("utils.whichkey-plugin")
 require("utils.hop-plugin")
+require("utils.rnvimr-plugin")
 require("utils.tagbar-plugin")
 require("utils.nvim-autopairs-plugin")
 require("utils.lualine-plugin")
