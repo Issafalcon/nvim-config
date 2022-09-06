bb269c0bf293951824cafa3455b6acca9f48945a
Adam Figgins <adam.figgins@nweh.co.uk>
2022-09-06 10:29:05 +0100

    Lsp-saga - Removing the lightbulb

--- lua/lsp/lsp-saga-plugin.lua
+++ lua/lsp/lsp-saga-plugin.lua
@@ -3,4 +3,11 @@ if not status_ok then
   return
 end
 
-saga.init_lsp_saga()
+saga.init_lsp_saga({
+  code_action_lightbulb = {
+    enable = false,
+    enable_in_insert = false,
+    cache_code_action = false,
+    sign = false
+  },
+})
