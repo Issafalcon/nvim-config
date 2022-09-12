8ddada0d52d7105cafe69b41d976d89fbcbe56df
Adam Figgins <atfiggins@gmail.com>
2022-09-12 20:07:07 +0100

    Neotest - Making the icons pretty

--- lua/testing/neotest-plugin.lua
+++ lua/testing/neotest-plugin.lua
@@ -2,6 +2,7 @@ local opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 require("neotest").setup({
+  -- log_level = 1, -- For verbose logs
   adapters = {
     require("neotest-python")({
       dap = { justMyCode = false },
@@ -14,6 +15,18 @@ require("neotest").setup({
     require("neotest-jest")({
       jestCommand = "npm test --",
     }),
+  },
+  icons = {
+    expanded = "",
+    child_prefix = "",
+    child_indent = "",
+    final_child_prefix = "",
+    non_collapsible = "",
+    collapsed = "",
+    passed = "",
+    running = "",
+    failed = "",
+    unknown = "",
   },
 })
 
