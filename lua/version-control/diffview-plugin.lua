ced5a82d32850974a516cd3cb6b92f894b778416
Adam Figgins <adam.figgins@nweh.co.uk>
2022-10-13 16:19:07 +0100

    Config - Fixing mappings object reference

--- lua/version-control/diffview-plugin.lua
+++ lua/version-control/diffview-plugin.lua
@@ -16,10 +16,10 @@ require("diffview").setup({
     -- The `view` bindings are active in the diff buffers, only when the current
     -- tabpage is a Diffview.
     view = {
-      [maps.diffview.view.select_next_entry] = cb("select_next_entry"), -- Open the diff for the next file
-      [maps.diffview.view.select_prev_entry] = cb("select_prev_entry"), -- Open the diff for the previous file
-      [maps.diffview.view.focus_files] = cb("focus_files"), -- Bring focus to the files panel
-      [maps.diffview.view.toggle_files] = cb("toggle_files"), -- Toggle the files panel.
+      [maps.diffview.select_next_entry] = cb("select_next_entry"), -- Open the diff for the next file
+      [maps.diffview.select_prev_entry] = cb("select_prev_entry"), -- Open the diff for the previous file
+      [maps.diffview.focus_files] = cb("focus_files"), -- Bring focus to the files panel
+      [maps.diffview.toggle_files] = cb("toggle_files"), -- Toggle the files panel.
     },
     file_panel = {
       ["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
@@ -34,10 +34,10 @@ require("diffview").setup({
       ["U"] = cb("unstage_all"), -- Unstage all entries.
       ["X"] = cb("restore_entry"), -- Restore entry to the state on the left side.
       ["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
-      [maps.diffview.view.select_next_entry] = cb("select_next_entry"), -- Open the diff for the next file
-      [maps.diffview.view.select_prev_entry] = cb("select_prev_entry"), -- Open the diff for the previous file
-      [maps.diffview.view.focus_files] = cb("focus_files"), -- Bring focus to the files panel
-      [maps.diffview.view.toggle_files] = cb("toggle_files"), -- Toggle the files panel.
+      [maps.diffview.select_next_entry] = cb("select_next_entry"), -- Open the diff for the next file
+      [maps.diffview.select_prev_entry] = cb("select_prev_entry"), -- Open the diff for the previous file
+      [maps.diffview.focus_files] = cb("focus_files"), -- Bring focus to the files panel
+      [maps.diffview.toggle_files] = cb("toggle_files"), -- Toggle the files panel.
     },
   },
 })
