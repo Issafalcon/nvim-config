088d59bba77a7051eaf41d74092b3b4d9fe5c8ef
Adam Figgins <adam.figgins@nweh.co.uk>
2022-12-15 11:02:30 +0000

    Mason - Adding fallback provider if primary is blocked (VPN)

--- lua/plugin-configs/mason.lua
+++ lua/plugin-configs/mason.lua
@@ -4,6 +4,10 @@ if not mason then
 end
 
 mason.setup({
+  providers = {
+    "mason.providers.registry-api",
+    "mason.providers.client",
+  },
   ui = {
     icons = {
       package_installed = "✓",
