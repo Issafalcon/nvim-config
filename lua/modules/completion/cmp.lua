84f10ab64392b538d81812ffb4087e68c30a0ea8
Adam Figgins <adam.figgins@nweh.co.uk>
2023-02-10 08:42:53 +0000

    fix(latex): Adds options back in so latex compilation works

--- lua/modules/completion/cmp.lua
+++ lua/modules/completion/cmp.lua
@@ -26,10 +26,6 @@ local cmp_spec = {
   config = function()
     -- code
     local cmp = require("cmp")
-    -- local function has_words_before()
-    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    -- end
 
     local sources = {
       nvim_lsp = { name = "nvim_lsp", priority = 1000, keyword_length = 2 },
@@ -123,8 +119,6 @@ local cmp_spec = {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          -- elseif has_words_before() then
-          --   cmp.complete()
           else
             fallback()
           end
--- lua/modules/tex/spec.lua
+++ lua/modules/tex/spec.lua
@@ -11,6 +11,28 @@ local vimtex_spec = {
   "lervag/vimtex",
   ft = "tex",
   keys = fignvim.mappings.make_lazy_keymaps(vimtex_keys, true),
+  init = function()
+    fignvim.config.set_vim_opts({
+      g = {
+        vimtex_view_method = "zathura", -- set default viewer for vimtex
+        vimtex_view_general_viewer = "zathura", -- set default viewer for vimtex
+        vimtex_compiler_latexmk = {
+          build_dir = "",
+          callback = 1,
+          continuous = 1,
+          executable = "latexmk",
+          hooks = {},
+          options = {
+            "-verbose",
+            "-file-line-error",
+            "-synctex=1",
+            "-interaction=nonstopmode",
+            "-shell-escape",
+          },
+        },
+      },
+    })
+  end,
 }
 
 return fignvim.module.enable_registered_plugins({
