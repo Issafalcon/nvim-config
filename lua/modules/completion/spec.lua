ae07c7ecc5be3aa83fb2d83019295c39aa343997
Adam Figgins <adam.figgins@nweh.co.uk>
2023-03-09 11:49:32 +0000

    fix(copilot): Fixes issue where copilot still maps tab
    - Errors when using tab with copilot fixed by moving the option
      configuration to be set in the init part of pacakge loading

--- lua/modules/completion/spec.lua
+++ lua/modules/completion/spec.lua
@@ -11,14 +11,14 @@ local copilot_spec = {
   "github/copilot.vim",
   event = "InsertEnter",
   keys = fignvim.mappings.make_lazy_keymaps(copilot_keys, true),
-  config = function()
-    fignvim.mappings.make_legendary_keymaps(copilot_keys, false)
+  init = function()
     fignvim.config.set_vim_opts({
       g = {
         copilot_no_tab_map = true, -- Disable tab mapping in insert mode when using copilot (so you can override the default mapping)
       },
     })
   end,
+  config = function() fignvim.mappings.make_legendary_keymaps(copilot_keys, false) end,
 }
 
 return fignvim.module.enable_registered_plugins({
