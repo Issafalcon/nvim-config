1a4eee919992990063b2c0d28268376bf2420f77
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2023-06-27 14:48:51 +0100

    fix(omnisharp): Fixes LSP cmd when running windows
    - Spaces in user name in windows leads to failed omnisharp startup
      command
    - This fixes that for windwos

--- lua/lsp/lsp_servers/omnisharp.lua
+++ lua/lsp/lsp_servers/omnisharp.lua
@@ -1,19 +1,25 @@
 M = {}
-M.opts = {
+local cmd
+
+if vim.fn.has("win32") == 1 then
+  cmd = { "cmd.exe", "/C", "omnisharp" }
+else
   cmd = {
     "dotnet",
-    fignvim.path.concat({ vim.fn.stdpath("data"), "mason", "packages", "omnisharp", "OmniSharp.dll" }),
-  },
+    fignvim.path.concat({ vim.fn.stdpath("data"), "mason", "packages", "omnisharp", "omnisharp.cmd" }),
+  }
+end
+
+M.opts = {
+  cmd = cmd,
 }
 
 local extended_definitions_ok, extended_definitions = pcall(require, "omnisharp_extended")
 -- local neosharper = fignvim.plug.load_module_file("neosharper")
 
-if extended_definitions then
-  M.opts.handlers = {
-    ["textDocument/definition"] = extended_definitions.handler,
-  }
-end
+if extended_definitions then M.opts.handlers = {
+  ["textDocument/definition"] = extended_definitions.handler,
+} end
 
 -- if neosharper then
 --   M.opts.on_attach = function(client, bufnr)
