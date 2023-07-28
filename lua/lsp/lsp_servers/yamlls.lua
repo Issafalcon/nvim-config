c836b9b48e90f83ab23b8ebd75565408f4c7a990
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2023-07-28 14:51:22 +0100

    feat(shemastore): Adds yaml schema with custom azure pipelines schema

--- lua/lsp/lsp_servers/yamlls.lua
+++ lua/lsp/lsp_servers/yamlls.lua
@@ -1,12 +1,28 @@
 M = {}
+
+local function load_schemas()
+  local schemastore_ok, schemastore = pcall(require, "schemastore")
+  return schemastore_ok and schemastore.yaml.schemas() or {}
+end
+
 M.opts = {
   settings = {
     yaml = {
-      schemas = {
-        kubernetes = { "*.yaml", "/*.yaml" },
-      },
+      schemas = require("schemastore").yaml.schemas({
+        extra = {
+          {
+            description = "Azure DevOps Pipeline Yaml schema",
+            name = "azure-pipelines",
+            fileMatch = { "*.yml", "*.yaml", "*pipelines.yml", "*pipelines.yaml" },
+            url = "https://dev.azure.com/awsmtech/_apis/distributedtask/yamlschema",
+          },
+        },
+      }),
       schemaStore = {
-        enable = true,
+        -- Disable built-in schemaStore support if you want to use schemastore plugin
+        enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
       },
     },
   },
