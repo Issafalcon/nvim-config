b80bc683f947e8ae4bced99592614731a0656cf6
Adam Figgins <adam@pop-os.localdomain>
2024-05-03 22:35:33 +0100

    Dap - Updates .net stuff

--- lua/dap_configs.lua
+++ lua/dap_configs.lua
@@ -2,6 +2,37 @@ local M = {}
 
 local install_dir = fignvim.path.concat({ vim.fn.stdpath("data"), "mason" })
 
+-- Helper functions for .NET taken from https://github.com/mfussenegger/nvim-dap/wiki/Cookbook#making-debugging-net-easier
+vim.g.dotnet_build_project = function()
+  local default_path = vim.fn.getcwd() .. "/"
+  if vim.g["dotnet_last_proj_path"] ~= nil then default_path = vim.g["dotnet_last_proj_path"] end
+  local path = vim.fn.input("Path to your *proj file", default_path, "file")
+  vim.g["dotnet_last_proj_path"] = path
+  local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+  print("")
+  print("Cmd to execute: " .. cmd)
+  local f = os.execute(cmd)
+  if f == 0 then
+    print("\nBuild: ✔️ ")
+  else
+    print("\nBuild: ❌ (code: " .. f .. ")")
+  end
+end
+
+vim.g.dotnet_get_dll_path = function()
+  local request = function() return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file") end
+
+  if vim.g["dotnet_last_dll_path"] == nil then
+    vim.g["dotnet_last_dll_path"] = request()
+  else
+    if vim.fn.confirm("Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2) == 1 then
+      vim.g["dotnet_last_dll_path"] = request()
+    end
+  end
+
+  return vim.g["dotnet_last_dll_path"]
+end
+
 M.javascript = {
   {
     type = "pwa-node",
@@ -90,7 +121,10 @@ M.cs = {
     type = "netcoredbg",
     name = "launch - netcoredbg",
     request = "launch",
-    program = function() return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file") end,
+    program = function()
+      if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then vim.g.dotnet_build_project() end
+      return vim.g.dotnet_get_dll_path()
+    end,
   },
 }
 
