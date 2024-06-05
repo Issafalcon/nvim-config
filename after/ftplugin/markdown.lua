638b5c58b5f396176739c767d16c718f0c041617
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-06-05 08:48:26 +0100

    Markdown - Fixing ftplugin after for markdown

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,15 +1,13 @@
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
-end
+local opts = { noremap = true, silent = true, buffer = 0 }
 
-vim.cmd [[
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-]]
+vim.keymap.set("n", "<Tab>", "<Cmd>call search('\\[[^]]*\\]([^)]\\+)')<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", "<Cmd>call search('\\[[^]]*\\]([^)]\\+)', 'b')<CR>", opts)
+
+vim.o.wrap = true
+vim.o.spell = true
+
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then return end
 
 cmp.setup.buffer({
   sources = {
@@ -18,9 +16,7 @@ cmp.setup.buffer({
     {
       name = "buffer",
       option = {
-        get_bufnrs = function()
-          return vim.api.nvim_list_bufs()
-        end,
+        get_bufnrs = function() return vim.api.nvim_list_bufs() end,
       },
     },
     { name = "path" },
