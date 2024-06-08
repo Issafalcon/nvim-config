265eb70283f0aa52b0a58d233c7b80ed2b4ff92f
Adam Figgins <adam@pop-os.localdomain>
2024-06-08 09:32:00 +0100

    feat(dap): Enhanced dap picker for .net launch

--- lua/dap_configs.lua
+++ lua/dap_configs.lua
@@ -20,7 +20,25 @@ vim.g.dotnet_build_project = function()
 end
 
 vim.g.dotnet_get_dll_path = function()
-  local request = function() return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file") end
+  local request = function()
+    local label_fn = function(dll) return string.format("dll = %s", dll.short_path) end
+    local procs = {}
+    for _, csprojPath in ipairs(vim.fn.glob(vim.fn.getcwd() .. "**/*.csproj", false, true)) do
+      local name = vim.fn.fnamemodify(csprojPath, ":t:r")
+      for _, path in ipairs(vim.fn.glob(vim.fn.getcwd() .. "**/bin/**/" .. name .. ".dll", false, true)) do
+        table.insert(procs, {
+          path = path,
+          short_path = vim.fn.fnamemodify(path, ":p:."),
+        })
+      end
+    end
+
+    local co, ismain = coroutine.running()
+    local ui = require("dap.ui")
+    local pick = (co and not ismain) and ui.pick_one or ui.pick_one_sync
+    local result = pick(procs, "Select process: ", label_fn)
+    return result and result.path or require("dap").ABORT
+  end
 
   if vim.g["dotnet_last_dll_path"] == nil then
     vim.g["dotnet_last_dll_path"] = request()
