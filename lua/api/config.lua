7b18f77a7ae2108650a6379d8c43bd4f3ef316fb
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-06-24 16:33:08 +0100

    fix(clipboard): Speed up WSL clipboard (but slow when focus in and out)

--- lua/api/config.lua
+++ lua/api/config.lua
@@ -34,7 +34,28 @@ function fignvim.config.set_win32yank_wsl_as_clip()
       ["*"] = "win32yank.exe -o --lf",
     },
     cache_enabled = 1,
+    -- name = "wsl-clipboard",
+    -- copy = {
+    --   ["+"] = "clip.exe",
+    --   ["*"] = "clip.exe",
+    -- },
+    -- paste = {
+    --   ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    --   ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    -- },
   }
+
+  -- sync with system clipboard on focus
+  vim.api.nvim_create_autocmd({ "FocusGained" }, {
+    pattern = { "*" },
+    command = [[call setreg("@", getreg("+"))]],
+  })
+
+  -- sync with system clipboard on focus
+  vim.api.nvim_create_autocmd({ "FocusLost" }, {
+    pattern = { "*" },
+    command = [[call setreg("+", getreg("@"))]],
+  })
 end
 
 return fignvim.config
