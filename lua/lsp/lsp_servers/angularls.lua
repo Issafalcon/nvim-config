183366c9c1d4c1e148335c9ca47113aa9ad99f43
Adam Figgins <adam.figgins@wedo-digital.co.uk>
2024-09-25 15:14:48 +0100

    fix(ts_ls): Tsserver string renames

--- lua/lsp/lsp_servers/angularls.lua
+++ lua/lsp/lsp_servers/angularls.lua
@@ -1,13 +1,13 @@
 M = {}
 
-M.on_attach = function (client, bufnr)
-    local clients = vim.lsp.buf_get_clients(bufnr)
-    for _, other_client in pairs(clients) do
-      if other_client.name == "tsserver" then
-        -- Prevent tsserver rename duplication when angularls is in use
-        other_client.server_capabilities.rename = false
-      end
+M.on_attach = function(client, bufnr)
+  local clients = vim.lsp.buf_get_clients(bufnr)
+  for _, other_client in pairs(clients) do
+    if other_client.name == "ts_ls" then
+      -- Prevent tsserver rename duplication when angularls is in use
+      other_client.server_capabilities.rename = false
     end
+  end
 end
 
 return M
