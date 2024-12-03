cb8c43b40e3a815e9e1e6a1691a2e5ed420d34df
Adam Figgins <adam_figgins@waters.com>
2024-12-03 09:29:16 +0000

    Copilot - Proxy strict ssl set to false

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -20,6 +20,7 @@ return {
       fignvim.config.set_vim_opts({
         g = {
           copilot_no_tab_map = true, -- Disable tab mapping in insert mode when using copilot (so you can override the default mapping)
+          copilot_proxy_strict_ssl = false,
         },
       })
     end,
