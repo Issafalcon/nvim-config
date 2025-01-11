50fd3991770cf806a00ea8d487c5bf4897452f1e
Adam Figgins <atfiggins@gmail.com>
2025-01-11 22:07:38 +0000

    Rocks - Setup for rocks

---
+++ lua/plugin-rocks/catppuccin.lua
@@ -0,0 +1,20 @@
+-- Colourschemes
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = false,
+  term_colors = true,
+  integrations = {
+    telescope = true,
+    aerial = true,
+    gitsigns = true,
+    cmp = true,
+    nvimtree = true,
+    mason = true,
+  },
+  dim_inactive = {
+    enabled = true,
+    shade = "dark",
+    percentage = 0.10,
+  },
+})
+vim.cmd.colorscheme("catppuccin")
