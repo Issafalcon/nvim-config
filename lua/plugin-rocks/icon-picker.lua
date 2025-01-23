b98a53b0e808b7a9ba74a8c1f541b6a1083036f1
Adam Figgins <adam_figgins@waters.com>
2025-01-23 16:32:49 +0000

    Rocks - Migrating more small plugins

---
+++ lua/plugin-rocks/icon-picker.lua
@@ -0,0 +1,6 @@
+-- https://github.com/ziontee113/icon-picker.nvim
+require("icon-picker").setup({
+  opts = {
+    disable_legacy_commands = true,
+  },
+})
--- lua/plugins/icon-picker.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "ziontee113/icon-picker.nvim",
-    cmd = { "IconPickerYank", "IconPickerNormal", "IconPickerInsert" },
-    opts = {
-      disable_legacy_commands = true,
-    },
-  },
-}
