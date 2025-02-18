0724ca24ecf07e7b39ea51a8486116c579a23aad
Adam Figgins <adam_figgins@waters.com>
2025-02-18 17:39:00 +0000

    Git - Starting git plugin migrations

---
+++ lua/git/plugin-config/gitsigns.lua
@@ -0,0 +1,47 @@
+---@type FigNvimPluginConfig
+local M = {}
+
+M.lazy_opts = {
+  signs = {
+    add = { text = "▎" },
+    change = { text = "▎" },
+    delete = { text = "▎" },
+    topdelete = { text = "契" },
+    changedelete = { text = "▎" },
+  },
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 300,
+    follow_files = true,
+  },
+  attach_to_untracked = true,
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+    ignore_whitespace = false,
+  },
+  sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = "single",
+    style = "minimal",
+    relative = "cursor",
+    row = 0,
+    col = 1,
+  },
+  on_attach = function(bufnr)
+    -- for _, keymap in ipairs(gitsigns_mappings) do
+    --   local buf_specific_opts = fignvim.table.default_tbl({ buffer = bufnr }, keymap[4])
+    --   vim.keymap.set(keymap[1], keymap[2], keymap[3], buf_specific_opts)
+    -- end
+  end,
+}
+return M
