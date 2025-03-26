9954a8cdd5fd36871ea48d561fd6f763a4828584
Adam Figgins <adam_figgins@waters.com>
2025-03-26 16:47:47 +0000

    Qmk -Plugin configured

--- lua/misc/plugin-configs/qmk.lua
+++ lua/misc/plugin-configs/qmk.lua
@@ -2,8 +2,14 @@
 local M = {}
 
 M.lazy_opts = {
-  name = "LAYOUT_sofle_rgb_neovim",
-  layout = {},
+  name = "LAYOUT_sofle_rgb_issafalcon",
+  layout = {
+    "x x x x x x _ _ _ _ _ x x x x x x",
+    "x x x x x x _ _ _ _ _ x x x x x x",
+    "x x x x x x _ _ _ _ _ x x x x x x",
+    "x x x x x x x _ _ _ x x x x x x x",
+    "_ _ _ x x x x x _ x x x x x _ _ _",
+  },
 }
 
 return M
