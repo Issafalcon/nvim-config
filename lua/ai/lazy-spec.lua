ebee7655a42818c5580dc684d86c69ca8fd410d6
Adam Figgins <adam_figgins@waters.com>
2025-05-08 16:29:20 +0100

    Avante - Adding plugin

--- lua/ai/lazy-spec.lua
+++ lua/ai/lazy-spec.lua
@@ -1,5 +1,6 @@
 --- @type FigNvimPluginConfig
 local copilot = require("ai.plugin-config.copilot")
+local avante_config = require("ai.plugin-config.avante")
 
 local copilot_keys = {
   {
@@ -20,5 +21,12 @@ return {
     event = "InsertEnter",
     keys = fignvim.mappings.make_lazy_keymaps(copilot_keys, true),
     init = copilot.lazy_init,
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    opts = avante_config.lazy_opts,
+    build = "make",
   },
 }
---
+++ lua/ai/plugin-config/avante.lua
@@ -0,0 +1,8 @@
+---@type FigNvimPluginConfig
+local M = {}
+
+M.lazy_opts = {
+  provider = "copilot",
+}
+
+return M
