39c0ffe0fc32bb7c02242fc6a0fca737439be7c7
Adam Figgins <adam_figgins@waters.com>
2025-05-12 16:42:48 +0100

    lsp - reducing impact of codelens refresh

--- lua/lsp/api/init.lua
+++ lua/lsp/api/init.lua
@@ -126,7 +126,7 @@ function fignvim.lsp.setup(opts)
   if opts.codelens.enabled and vim.lsp.codelens then
     fignvim.lsp.on_supports_method("textDocument/codeLens", function(_, buffer)
       vim.lsp.codelens.refresh()
-      vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
+      vim.api.nvim_create_autocmd({ "BufEnter", "InsertLeave" }, {
         buffer = buffer,
         callback = vim.lsp.codelens.refresh,
       })
