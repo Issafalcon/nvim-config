61b5ac75b0f20c9189415570be69c92b2ee5d10b
Adam Figgins <atfiggins@gmail.com>
2025-06-05 22:02:30 +0100

    Snacks - Fixes lazygit mappings

--- lua/misc/lazy-spec.lua
+++ lua/misc/lazy-spec.lua
@@ -1,9 +1,13 @@
 local snacks_config = require("misc.plugin-configs.snacks")
 local qmk_config = require("misc.plugin-configs.qmk")
+local git_mappings = require("keymaps").Git
 
 return {
   {
     "folke/snacks.nvim",
+    keys = fignvim.mappings.make_lazy_keymaps({
+      git_mappings.ToggleLazygit,
+    }, true),
     priority = 1000,
     lazy = false,
     opts = snacks_config.lazy_opts,
