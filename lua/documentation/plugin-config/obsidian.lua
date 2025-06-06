eb84ffd4fdeb9d8b9e5736014f17eea06aba9fb0
Adam Figgins <adam_figgins@waters.com>
2025-06-06 16:27:34 +0100

    Obsidian - Removes annoying keymaps

--- lua/documentation/plugin-config/obsidian.lua
+++ lua/documentation/plugin-config/obsidian.lua
@@ -37,6 +37,15 @@ M.lazy_opts = {
     folder = "Daily",
     template = "DailyTemplate.md",
   },
+  mappings = {
+    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    ["gf"] = {
+      action = function()
+        return require("obsidian").util.gf_passthrough()
+      end,
+      opts = { noremap = false, expr = true, buffer = true },
+    },
+  },
 }
 
 return M
