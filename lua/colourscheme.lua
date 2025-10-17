fb1d7c84113c783ebb3bcccf9e340d11672d3316
Adam Figgins <adam_figgins@waters.com>
2025-10-17 13:37:41 +0100

    Mason after lsp

--- lua/colourscheme.lua
+++ lua/colourscheme.lua
@@ -3,6 +3,8 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
+local M = {}
+
 M.colours = {
   none = "NONE",
   fg = "#abb2bf",
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,6 @@
 require("plugins.plenary")
 require("plugins.catppuccin")
-require("plugins.mason")
 require("plugins.lsp")
+require("plugins.mason")
 require("plugins.conform")
 require("plugins.telescope")
