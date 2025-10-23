ca0d59cd8c84567d251a2cdf76b33b8105c4917b
Adam Figgins <adam_figgins@waters.com>
2025-10-23 16:57:54 +0100

    More migration of plugins

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,41 @@
+vim.pack.add({
+  { src = "https://github.com/akinsho/toggleterm.nvim" },
+})
+
+require("toggleterm").setup({
+  size = 10,
+  open_mapping = [[<F7><F7>]],
+  shading_factor = 2,
+  direction = "float",
+  float_opts = {
+    border = "curved",
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+})
+
+vim.keymap.set("n", "<F7>n", function()
+  fignvim.term.toggle_term_cmd("node")
+end, { desc = "ToggleTerm with Node" })
+
+vim.keymap.set("n", "<F7>p", function()
+  fignvim.term.toggle_term_cmd("python")
+end, { desc = "ToggleTerm with Python" })
+
+vim.keymap.set("n", "<F7>f", "<cmd>ToggleTerm direction=float<cr>", { desc = "ToggleTerm in floating window" })
+vim.keymap.set(
+  "n",
+  "<F7>h",
+  "<cmd>ToggleTerm size=10 direction=horizontal<cr>",
+  { desc = "ToggleTerm in horizontal split" }
+)
+vim.keymap.set(
+  "n",
+  "<F7>v",
+  "<cmd>ToggleTerm size=80 direction=vertical<cr>",
+  { desc = "ToggleTerm in vertical split" }
+)
+vim.keymap.set({ "n", "t" }, "<F7><F7>", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm" })
+vim.keymap.set({ "n", "t" }, "<C-'>", "<cmd>ToggleTerm<cr>", { desc = "ToggleTerm" })
---
+++ lua/plugins/vim-terraform.lua
@@ -0,0 +1,4 @@
+vim.pack.add({
+  { src = "https://github.com/juliosueiras/vim-terraform-completion" },
+  { src = "https://github.com/hashivim/vim-terraform" },
+})
