9b00954a6a4f45ae5e68ec1642fe1cd1e6f63ec2
Adam Figgins <adam_figgins@waters.com>
2025-11-05 17:42:42 +0000

    Adding jira mcp

--- lua/plugins/mcphub.lua
+++ lua/plugins/mcphub.lua
@@ -5,4 +5,12 @@ vim.pack.add({
 require("mcphub").setup({
   -- Your configuration here
   config = vim.fn.expand("~/.config/mcphub/servers.json"),
+  global_env = {
+    "CONFLUENCE_URL",
+    "CONFLUENCE_USERNAME",
+    "CONFLUENCE_API_TOKEN",
+    "JIRA_URL",
+    "JIRA_USERNAME",
+    "JIRA_API_TOKEN",
+  },
 })
