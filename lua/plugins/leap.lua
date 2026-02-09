f71aa61bdeb616170f73504858c1f0daa78213f5
Adam Figgins <adam_figgins@waters.com>
2026-02-09 12:03:12 +0000

    Leap - Fixing deprecate mappings

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,6 +2,6 @@ vim.pack.add({
   { src = "https://github.com/ggandor/leap.nvim" },
 })
 
-vim.keymap.set("n", "<C-m>", "<plug>(leap-forward-to)", { desc = "Leap: Forward to" })
-vim.keymap.set("n", "<C-n>", "<plug>(leap-backward-to)", { desc = "Leap: Backward to" })
-vim.keymap.set("n", "gs", "<plug>(leap-cross-window)", { desc = "Leap: Across all windows" })
+vim.keymap.set("n", "<C-m>", "<plug>(leap-forward)", { desc = "Leap: Forward to" })
+vim.keymap.set("n", "<C-n>", "<plug>(leap-backward)", { desc = "Leap: Backward to" })
+vim.keymap.set("n", "gs", "<plug>(leap-anywhere)", { desc = "Leap: Across all windows" })
