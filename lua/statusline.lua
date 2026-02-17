ba2454ad656e652b1528a30ace5cc611a73cdc34
Adam Figgins <adam_figgins@waters.com>
2026-02-17 16:48:41 +0000

    Statusline - Kindof there. Colours are off

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -2,23 +2,27 @@ local icons = require('icons')
 
 local M = {}
 
+-- Unicode slant separators
+local SEP_RIGHT = ''
+local SEP_LEFT = ''
+
 -- Helper: get current mode name and highlight group
 local function mode_info()
   local mode_map = {
-    n = { 'NORMAL', 'StatuslineModeNormal' },
-    i = { 'INSERT', 'StatuslineModeInsert' },
-    v = { 'VISUAL', 'StatuslineModeVisual' },
-    V = { 'V-LINE', 'StatuslineModeVisual' },
-    ['\22'] = { 'V-BLOCK', 'StatuslineModeVisual' },
-    c = { 'COMMAND', 'StatuslineModeCommand' },
-    s = { 'SELECT', 'StatuslineModeOther' },
-    S = { 'S-LINE', 'StatuslineModeOther' },
-    R = { 'REPLACE', 'StatuslineModeOther' },
-    t = { 'TERMINAL', 'StatuslineModeOther' },
+    n = { 'NORMAL', 'StatuslineModeNormal', 'StatuslineModeSeparatorNormal' },
+    i = { 'INSERT', 'StatuslineModeInsert', 'StatuslineModeSeparatorInsert' },
+    v = { 'VISUAL', 'StatuslineModeVisual', 'StatuslineModeSeparatorVisual' },
+    V = { 'V-LINE', 'StatuslineModeVisual', 'StatuslineModeSeparatorVisual' },
+    ['\22'] = { 'V-BLOCK', 'StatuslineModeVisual', 'StatuslineModeSeparatorVisual' },
+    c = { 'COMMAND', 'StatuslineModeCommand', 'StatuslineModeSeparatorCommand' },
+    s = { 'SELECT', 'StatuslineModeOther', 'StatuslineModeSeparatorOther' },
+    S = { 'S-LINE', 'StatuslineModeOther', 'StatuslineModeSeparatorOther' },
+    R = { 'REPLACE', 'StatuslineModeOther', 'StatuslineModeSeparatorOther' },
+    t = { 'TERMINAL', 'StatuslineModeOther', 'StatuslineModeSeparatorOther' },
   }
   local mode = vim.api.nvim_get_mode().mode
-  local info = mode_map[mode] or { mode, 'StatuslineModeOther' }
-  return info[1], info[2]
+  local info = mode_map[mode] or { mode, 'StatuslineModeOther', 'StatuslineModeSeparatorOther' }
+  return info[1], info[2], info[3]
 end
 
 -- Helper: get git branch (if available)
@@ -30,17 +34,37 @@ local function git_branch()
   return ''
 end
 
--- Helper: get diagnostics summary
+-- Helper: get git diff summary with highlights
+local function git_diff()
+  local signs = vim.b.gitsigns_status_dict or {}
+  local added = signs.added or 0
+  local changed = signs.changed or 0
+  local removed = signs.removed or 0
+  local out = {}
+  if added > 0 then table.insert(out, string.format('%%#diffAdded#%s%d', icons.git.added, added)) end
+  if changed > 0 then table.insert(out, string.format('%%#diffChanged#%s%d', icons.git.modified, changed)) end
+  if removed > 0 then table.insert(out, string.format('%%#diffRemoved#%s%d', icons.git.removed, removed)) end
+  return #out > 0 and (' ' .. table.concat(out, ' ')) or ''
+end
+
+-- Helper: get diagnostics summary with highlights
 local function diagnostics()
   local counts = { error = 0, warn = 0, info = 0, hint = 0 }
   for k in pairs(counts) do
     counts[k] = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity[k:upper()] })
   end
   local out = {}
-  for k, v in pairs(counts) do
-    if v > 0 then
-      table.insert(out, string.format('%s %d', icons.diagnostics[k], v))
-    end
+  if counts.error > 0 then
+    table.insert(out, string.format('%%#DiagnosticError#%s %d', icons.diagnostics.error, counts.error))
+  end
+  if counts.warn > 0 then
+    table.insert(out, string.format('%%#DiagnosticWarn#%s %d', icons.diagnostics.warn, counts.warn))
+  end
+  if counts.info > 0 then
+    table.insert(out, string.format('%%#DiagnosticInfo#%s %d', icons.diagnostics.info, counts.info))
+  end
+  if counts.hint > 0 then
+    table.insert(out, string.format('%%#DiagnosticHint#%s %d', icons.diagnostics.hint, counts.hint))
   end
   return #out > 0 and (' ' .. table.concat(out, ' ')) or ''
 end
@@ -64,14 +88,17 @@ local function file_info()
   return string.format(' %s%s%s ', name, modified, readonly)
 end
 
--- Helper: location
+-- Helper: location (line/total_lines (percent%))
 local function location()
-  return string.format(' %3d:%-2d ', unpack(vim.api.nvim_win_get_cursor(0)))
+  local line = vim.fn.line('.');
+  local total = vim.fn.line('$');
+  local percent = math.floor((line / total) * 100)
+  return string.format(' %d/%d (%d%%%%) ', line, total, percent)
 end
 
 -- LSP info click handler
-local function lsp_info_click()
-  vim.cmd([[lua require('statusline').show_lsp_health() ]])
+function M.lsp_info_click()
+  require('statusline').show_lsp_health()
 end
 
 -- Show floating window with :checkhealth vim.lsp
@@ -95,19 +122,39 @@ end
 
 -- Compose the statusline string
 function M.statusline()
-  local mode, mode_hl = mode_info()
+  local mode, mode_hl, sep_hl = mode_info()
   local stl = ''
+
+  -- Mode
   stl = stl .. '%#' .. mode_hl .. '# ' .. mode .. ' '
-  stl = stl .. '%#StatusLine#'
-  stl = stl .. git_branch()
-  stl = stl .. file_info()
-  stl = stl .. '%=' -- align right
+  stl = stl .. '%#' .. sep_hl .. '#' .. SEP_RIGHT
+
+  -- Git branch
+  stl = stl .. '%#StatuslineTitle#' .. git_branch()
+  stl = stl .. '%#StatuslineTitle#' .. SEP_RIGHT
+
+  -- Git diff
+  stl = stl .. git_diff()
+  stl = stl .. '%#diffAdded#' .. SEP_RIGHT
+
+  -- File info
+  stl = stl .. '%#StatuslineItalic#' .. file_info()
+  stl = stl .. '%#StatuslineItalic#' .. SEP_RIGHT
+
+  -- Align right
+  stl = stl .. '%=' 
+
+  -- Diagnostics
   stl = stl .. diagnostics()
+  stl = stl .. '%#DiagnosticInfo#' .. SEP_LEFT
+
   -- LSP info: clickable
-  stl = stl .. '%@v:lua.require"statusline".lsp_info_click@'
-  stl = stl .. lsp_servers()
-  stl = stl .. '%T'
-  stl = stl .. location()
+  stl = stl .. '%#StatuslineSpinner#%@v:lua.require"statusline".lsp_info_click@' .. lsp_servers() .. '%T'
+  stl = stl .. '%#StatuslineSpinner#' .. SEP_LEFT
+
+  -- Location
+  stl = stl .. '%#StatuslineTitle#' .. location()
+
   return stl
 end
 
