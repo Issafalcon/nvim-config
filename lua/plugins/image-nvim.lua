b3746e3496df98bf5e2bf60ffbfa87df4fce7d5d
issafalcon <atfiggins@gmail.com>
2026-04-19 15:28:00 +0100

    Image - Removing markdown integration

--- lua/plugins/image-nvim.lua
+++ lua/plugins/image-nvim.lua
@@ -8,11 +8,11 @@ vim.pack.add({
 vim.api.nvim_create_autocmd("UIEnter", {
   callback = function()
     require("image").setup({
-      backend = "kitty",
+      backend = "ueberzug",
       processor = "magick_cli",
       integrations = {
         markdown = {
-          enabled = true,
+          enabled = false,
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = false,
